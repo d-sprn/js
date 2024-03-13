051d30b90191b0bd5e5bb431e42dd32a0236db07
d-sprn <126406171+d-sprn@users.noreply.github.com>
2024-03-13 15:40:42 +0200

    js_step_1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,20 @@
-'use strict'
+'use strict';
+
+const numberOfFilms = +prompt('How many films did you seen?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Last movie'),
+    b = prompt('Rate last movie'),
+    c = prompt('Last movie'),
+    d = prompt('Rate the movie');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
