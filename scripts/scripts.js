1696db2f9fac38d2a181ee297f8a06981bd7b16d
d-sprn <126406171+d-sprn@users.noreply.github.com>
2024-03-13 16:58:30 +0200

    js_step_5

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,72 +1,29 @@
 'use strict';
 
-
-
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-
-    start: function () {
-        personalMovieDB.count = +prompt('How many films did you seen?', '');
-
-        while (personalMovieDB.count === '' || personalMovieDB.count === null || isNan(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt('How many films did you seen?', '');
-        }
-    },
-
-    rememberMyFilms: function () {
-        for (let i = 0; i < 2; i++) {
-            const a = prompt('Last movie'),
-                b = prompt('Rate last movie');
-
-            if (a != null && b != null && a !== '' && b !== '' && a.length < 50) {
-                personalMovieDB.movies[a] = b;
-                console.log('done');
-            } else {
-                console.log('error');
-                i--;
-            }
-        }
-    },
-
-    detectPersonalLevel: function () {
-        if (personalMovieDB.count < 10) {
-            console.log("Просмотрено довольно мало фильмов");
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-            console.log("Вы классический зритель");
-        } else if (personalMovieDB.count >= 30) {
-            console.log("Вы киноман");
-        } else {
-            console.log("Произошла ошибка");
-        }
-    },
-
-    showMyDB: function (hidden) {
-        if (!hidden) {
-            console.log(personalMovieDB)
-        }
-    },
-
-    writeYourGenres: function () {
-        for (let i = 1; i <= 3; i++) {
-            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-
-            if (genre === '' || genre === null) {
-                console.log('Вы ввели некорректные данные или не ввели их вовсе');
-                i--;
-            } else {
-                personalMovieDB.genres[i - 1] = genre
-            }
-        }
-
-        personalMovieDB.genres.forEach((item, i) => {
-            console.log(`Любимый жанр ${i + 1} - это ${item}`)
-        })
-    }
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      genre = document.querySelector('.promo__genre'),
+      poster = document.querySelector('.promo__bg'),
+      movieList = document.querySelector('.promo__interactive')
 
-console.log(personalMovieDB);
+adv.forEach(item => item.remove())
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = "";
+movieDB.movies.sort();
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
