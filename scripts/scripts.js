52d114b2886ec37ba8802baf67ba0f7ba3768830
d-sprn <126406171+d-sprn@users.noreply.github.com>
2024-03-13 15:48:08 +0200

    js_step_2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,12 +9,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Last movie'),
-    b = prompt('Rate last movie'),
-    c = prompt('Last movie'),
-    d = prompt('Rate the movie');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Last movie'),
+        b = prompt('Rate last movie');
 
-console.log(personalMovieDB)
+    if (a != null && b != null && a != '' && b != '' && a.length<50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error')
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+
+console.log(personalMovieDB);
